6042179ad7c35c795869497309b05b4b437fb83e
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-02 08:36:18 -0400

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Benjamin Lavallee on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
