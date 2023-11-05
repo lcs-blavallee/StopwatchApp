7166ae8d8d92b467d14b67594464ff38b2520c93
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-05 11:54:57 -0500

    Finished product

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -14,9 +14,12 @@ struct ContentView: View {
             // First layer (backkground)
             Color.black
                 .ignoresSafeArea()
-            
+                
             // Second Layer (rest of interface)
             VStack {
+                
+                Spacer()
+                
                 Text("00:09.96")
                     .font(Font.system(size: 90, weight: .thin))
                     .foregroundStyle(.white)
@@ -31,6 +34,52 @@ struct ContentView: View {
                     
                     CircleButtonView(buttonColor: Color("Dark Green"), label: "Start", labelColor: .green)
                 }
+                // List of times
+                    List {
+                        Group {
+                            HStack {
+                                Text("Lap 5")
+                                Spacer()
+                                Text("00:00.98")
+                            }
+                            
+                            HStack {
+                                Text("Lap 4")
+                                    .foregroundStyle(.red)
+                                Spacer()
+                                Text("00:04.08")
+                                    .foregroundStyle(.red)
+                            }
+                           
+                            HStack {
+                                Text("Lap 3")
+                                    .foregroundStyle(.green)
+                                Spacer()
+                                Text("00:00.96")
+                                    .foregroundStyle(.green)
+                            }
+                           
+                            HStack {
+                                Text("Lap 2")
+                                Spacer()
+                                Text("00:02.76")
+                            }
+                          
+                            HStack {
+                                Text("Lap 1")
+                                Spacer()
+                                Text("00:01.16")
+                            }
+                            
+                        }
+                        // Remove inset from list items
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    }
+           
+                // Set the amount of vertical height we want this list to take up
+                .frame(height: 300)
+                // Adjust list style to match design
+                .listStyle(.plain)
             }
             .padding()
         }
--- StopwatchApp/StopwatchAppApp.swift
+++ StopwatchApp/StopwatchAppApp.swift
@@ -11,7 +11,37 @@ import SwiftUI
 struct StopwatchAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView(selection: Binding.constant(3)) {
+                
+                Text("World Clock")
+                    .tabItem {
+                        Image(systemName: "globe")
+                        Text("World Clock")
+                    }
+                    .tag(1)
+                        Text("Alarm")
+                     .tabItem {
+                        Image(systemName: "alarm.fill")
+                        Text("Alarm")
+                    }
+                     .tag(2)
+                
+                ContentView()
+                    .tabItem {
+                        Image(systemName: "stopwatch.fill")
+                        Text("Stopwatch")
+                    }
+                    .tag(3)
+                Text("Timer")
+                    .tabItem{
+                        Image(systemName: "timer")
+                        Text("Timer")
+                    }
+                    .tag(4)
+            }
+            
+            .accentColor(.orange)
+            .preferredColorScheme(.dark)
         }
     }
 }
